6003a1c98ed1bf05228735cd75ba7f5c67f860d2
luanfred <luanmatheussf4@gmail.com>
2024-07-16 00:00:28 -0300

    update test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println(somar(10, 10));
+        System.out.println("Hello World!");
     }
 
     public static int somar(int a, int b) {
